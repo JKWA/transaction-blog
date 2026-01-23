3887489b0659051378a0018175b95860d8dce8e8
Joseph Koski <joe.koski@gmail.com>
2026-01-23 11:33:24 -0800

    swap to working version

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Transaction.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:funx, "0.6.0"}
+      {:funx, github: "JKWA/funx", ref: "536cec6"}
     ]
   end
 end
